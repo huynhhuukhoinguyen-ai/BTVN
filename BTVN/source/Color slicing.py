7c2c422ab996fdffce7abb8859e1129cac54eb62
huynhhuukhoinguyen-ai <huynhhuukhoinguyen@gmail.com>
2026-09-16 01:00:49 +0700

    Update Color slicing.py

--- BTVN/source/Color slicing.py
+++ BTVN/source/Color slicing.py
@@ -2,11 +2,9 @@ import cv2
 import numpy as np
 import os
 os.makedirs("output", exist_ok=True)
-img = cv2.imread("anh3.png")
+img = cv2.imread("data/anh3.png")
 H, W, _ = img.shape
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray_bgr = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
 lower_red1 = np.array([0, 50, 50])
 upper_red1 = np.array([10, 255, 255])
 lower_red2 = np.array([170, 50, 50])
@@ -14,7 +12,8 @@ upper_red2 = np.array([180, 255, 255])
 mask1 = cv2.inRange(img_hsv, lower_red1, upper_red1)
 mask2 = cv2.inRange(img_hsv, lower_red2, upper_red2)
 mask = cv2.bitwise_or(mask1, mask2)
-img_sliced = np.where(mask[:, :, None] == 255, img, gray_bgr)
+black_bg = np.zeros_like(img)
+img_sliced = np.where(mask[:, :, None] == 255, img, black_bg)
 cv2.imshow('Original Image', img)
 cv2.imshow('Color Slicing Mask', mask)
 cv2.imshow('Color Slicing Result', img_sliced)
